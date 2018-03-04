12c404ac818a2aaecb906659185c6958d1c618af
Alexander Belyshev <32143234+avbelyshev@users.noreply.github.com>
2018-03-04 17:11:53 +0300

    Add null constraint answer.correct

--- db/migrate/20180303190249_add_null_constraint.rb
+++ db/migrate/20180303190249_add_null_constraint.rb
@@ -3,6 +3,7 @@ class AddNullConstraint < ActiveRecord::Migration[5.1]
     change_column_null :tests, :title, false
     change_column_null :questions, :body, false
     change_column_null :answers, :body, false
+    change_column_null :answers, :correct, false
     change_column_null :users, :email, false
     change_column_null :categories, :title, false
   end
