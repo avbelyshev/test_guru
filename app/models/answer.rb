4370cef2d810e0f32fc35bb2f0a29782fef65110
avbelyshev <avbelishev@gmail.com>
2018-03-13 02:04:18 +0300

    Add scopes & validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,16 @@
 class Answer < ApplicationRecord
-  belongs_to :question
+  MAX_ANSWERS_TO_QUESTION = 4
+
+  belongs_to :question, counter_cache: true
+
+  validates :body, presence: true
+  validate :answers_count_to_question
+
+  scope :correct, -> { where(correct: true) }
+
+  def answers_count_to_question
+    if question.answers_count >= MAX_ANSWERS_TO_QUESTION
+      errors.add(:question, 'Превышено максимальное количество ответов на вопрос')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
 
+  validates :email, presence: true
+
   def tests_by_level(level)
-    tests.where(level: level)
+    tests.by_level(level)
   end
 end
