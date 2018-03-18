9f84eb1dde8f5c874a4f5bdb57d4124455c08dac
avbelyshev <avbelishev@gmail.com>
2018-03-19 01:54:03 +0300

    Add helpers & views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,37 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: [:new, :create]
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
 
-  def index
-    render plain: @test.questions.inspect
+  def show; end
+
+  def new
+    @question = @test.questions.new
   end
 
-  def show
-    render plain: @question.body
-  end
-
-  def new; end
+  def edit; end
 
   def create
-    question = @test.questions.create(question_params)
+    @question = @test.questions.new(question_params)
 
-    render plain: "Создан вопрос #{question.body}"
+    if @question.save
+      redirect_to test_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to test_path(@question.test)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @question.delete
+    @question.destroy
 
-    render plain: 'Вопрос удален ('
+    redirect_to test_path(@question.test)
   end
 
   private
