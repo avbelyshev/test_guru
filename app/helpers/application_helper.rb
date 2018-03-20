6550fb321cbe4a2fa0ee530ca2e45aa268e61c38
avbelyshev <avbelishev@gmail.com>
2018-03-20 20:58:00 +0300

    Fix github_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to yield, "https://github.com/#{author}/#{repo}", target: '_blank'
+    link_to block_given? ? yield : repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 end
