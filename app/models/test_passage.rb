9f71eb9dc9a66c915200fba77b514cc2e5024456
avbelyshev <avbelishev@gmail.com>
2018-03-23 00:00:31 +0300

    Add test_passage callback & revision the views

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,19 +1,37 @@
 class TestPassage < ApplicationRecord
+  SUCCESS_RATE = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_current_question
+
+  def accept!(answer_ids)
+    self.correct_answers += 1 if right_answer?(answer_ids)
+
+    save!
+  end
+
+  def number_of_questions
+    test.questions.count
+  end
+
+  def current_question_number
+    test.questions.index(current_question) + 1
+  end
+
+  def right_answers_percent
+    (correct_answers / number_of_questions) * 100
+  end
 
   def completed?
     current_question.nil?
   end
 
-  def accept!(answer_ids)
-    self.correct_answers += 1 if right_answer?(answer_ids)
-
-    self.current_question = next_question
-    save!
+  def passed_successfully?
+    true if right_answers_percent >= SUCCESS_RATE
   end
 
   private
@@ -22,11 +40,12 @@ class TestPassage < ApplicationRecord
     self.current_question = test.questions.first if test.present?
   end
 
-  def right_answer?(answer_ids)
-    right_answers_count = right_answers.count
+  def before_update_set_current_question
+    self.current_question = next_question
+  end
 
-    (right_answers_count == right_answers.where(id: answer_ids).count) &&
-      right_answers_count == answer_ids.count
+  def right_answer?(answer_ids)
+    right_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def right_answers
