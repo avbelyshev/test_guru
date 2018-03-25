f037d72b11f2fad2eae407a0916bf53f4650d794
avbelyshev <avbelishev@gmail.com>
2018-03-25 18:13:55 +0300

    Add user & sessions controllers & authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
 
   root to: 'tests#index'
 
+  get 'users/new'
+  get 'sessions/new'
+
+  get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+
+  resources :users, only: :create
+  resources :sessions, only: :create
+
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320210741) do
+ActiveRecord::Schema.define(version: 20180324134327) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20180320210741) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
