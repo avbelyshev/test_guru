4dec00768999774fa3d168101142cb038408a807
avbelyshev <avbelishev@gmail.com>
2018-04-04 16:15:55 +0300

    Localization of other resources

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     super
-    flash[:notice] = "#{t('.welcome')}, #{current_user}"
+    flash[:notice] = t('.welcome', user_name: current_user)
   end
 end
