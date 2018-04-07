ef72026a434d98310a1abb9d78d62b2abf7b2918
avbelyshev <avbelishev@gmail.com>
2018-04-07 03:18:12 +0300

    Styling flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  FLASH_CLASS = {
+    error: 'alert alert-danger',
+    alert: 'alert alert-warning',
+    notice: 'alert alert-info',
+    success: 'alert alert-success'
+  }.freeze
+
   def current_year
     Date.current.year
   end
@@ -7,9 +14,13 @@ module ApplicationHelper
     link_to block_given? ? yield : repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
+  def flash_class(key)
+    FLASH_CLASS[key]
+  end
+
   def flash_messages
     flash.map do |key, message|
-      content_tag :p, message, class: "flash #{key}"
+      content_tag :div, message, class: flash_class(key.to_sym), role: 'alert'
     end.join.html_safe
   end
 end
