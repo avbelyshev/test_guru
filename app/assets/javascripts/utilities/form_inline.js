118b57602065e937dd1a52582bdc41cd1159a37b
avbelyshev <avbelishev@gmail.com>
2018-04-14 03:42:21 +0300

    Modification form_inline.js with jquery

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,11 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
-  var controls = document.querySelectorAll('.form-inline-link')
-
-  if (controls.length) {
-    for (var i = 0; i < controls.length; i++) {
-      controls[i].addEventListener('click', formInlineLinkHandler)
-    }
-  }
+  $('.form-inline-link').on('click', formInlineLinkHandler)
 
   var errors = document.querySelector('.resource-errors')
 
@@ -41,16 +35,12 @@ function formInlineHandler(testId) {
   }
 
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
 
-  if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide')
-    formInline.classList.remove('hide')
-    link.value = cancel_text
-  } else {
-    testTitle.classList.remove('hide')
-    formInline.classList.add('hide')
-    link.value = edit_text
-  }
+  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
+
+  $formInline.toggle()
+  $testTitle.toggle()
+
+  link.value = $formInline.is(':visible') ? cancel_text : edit_text
 }
