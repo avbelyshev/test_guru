12151832c90988c6af14984dd3f77512c462433f
avbelyshev <avbelishev@gmail.com>
2018-04-21 14:08:20 +0300

    Fix migration add_current_question_to_test_passages

--- db/migrate/20180320210741_add_current_question_to_test_passages.rb
+++ db/migrate/20180320210741_add_current_question_to_test_passages.rb
@@ -1,5 +1,5 @@
 class AddCurrentQuestionToTestPassages < ActiveRecord::Migration[5.1]
   def change
-    add_reference :test_passages, :current_question, foreign_key: true
+    add_column :test_passages, :current_question_id, :integer, index: true
   end
 end
