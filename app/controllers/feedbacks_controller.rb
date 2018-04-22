d2f2e07561853f96d44a2beda4a829c41a2df9c0
avbelyshev <avbelishev@gmail.com>
2018-04-22 20:21:32 +0300

    Fix feedback mailer & controller

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,6 +1,5 @@
 class FeedbacksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_admin
 
   def new
     @feedback = Feedback.new
@@ -10,7 +9,7 @@ class FeedbacksController < ApplicationController
     @feedback = current_user.feedbacks.new(feedback_params)
 
     if @feedback.save
-      FeedbackMailer.send_feedback(@admin, @feedback).deliver_now
+      FeedbackMailer.send_feedback(@feedback).deliver_now
 
       redirect_to root_path, notice: t('.success')
     else
@@ -19,10 +18,6 @@ class FeedbacksController < ApplicationController
   end
 
   private
-
-  def set_admin
-    @admin = User.find_by(type: 'Admin')
-  end
 
   def feedback_params
     params.require(:feedback).permit(:title, :body)
--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,8 +1,15 @@
 class FeedbackMailer < ApplicationMailer
-  def send_feedback(admin, feedback)
-    @admin = admin
+  before_action :set_admin
+
+  def send_feedback(feedback)
     @feedback = feedback
 
     mail to: @admin.email, from: @feedback.user.email, subject: @feedback.title
+  end
+
+  private
+
+  def set_admin
+    @admin = User.find_by(type: 'Admin')
   end
 end
